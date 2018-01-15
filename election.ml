9255769208e91a32de874869d1d37db7e0023ea6
Cosmin Nechifor <cosmin.nechifor96@gmail.com>
2018-01-15 21:08:08 +0200

    Sorting works

--- election.ml
+++ election.ml
@@ -38,16 +38,56 @@ val element = (List.nth (listFile, 0));
 val splitedList = splitter element;
 
 fun transform X = stripchars (X, "\n" );
+fun convert X = valOf(Int.fromString X);
+(*val b = transform ("1992\n");*)
+(* val b = convert("123");*)
 
-val b = transform ("1992\n");
+fun getParty(_, p, _) = p;
+fun getVotes(_, _, v) = v;
+fun getName(n, _, _) = n;
+
+(* type candidate = string * string * int; *)
+fun insert (n:(string * string * int)) nil = [n]
+| insert n (h::t) = if (getVotes h) < (getVotes n) then h:: insert n t else n::h::t;
+
+fun flatten nil = nil
+| flatten (h::t) = h @ flatten(t);
+
+fun sort nil = nil
+| sort (h::t) = insert h (sort t);
+
+fun sum nil = 0
+| sum(h::t) = h + sum t;
+
+fun last nil = 0
+| last (h::nil) = h
+| last (h::t) = last(t);
+
+
+fun isPartyP var a = if var = (getParty a) then true else false;
+
+fun filter f nil = nil
+|	filter f (h::t) = if f h then h::filter f t else filter f t;
 
 
 fun recursiv (nil, acc) = acc
-| recursiv(H::T, acc) = recursiv(T, ( List.nth((splitter H), 0), List.nth((splitter H), 1), transform (List.nth((splitter H),2)))::acc);
+| recursiv(H::T, acc) = recursiv(T, ( List.nth((splitter H), 0), List.nth((splitter H), 1), convert (transform (List.nth((splitter H),2))))::acc);
+
+fun isMember(name, party) = if (getParty(name) = party) then true else false;
 
 
 (* nthElement of a list *)
-val nthElement = (List.nth (listFile, 1));
-val splitedElement = splitter nthElement;
+(*val nthElement = (List.nth (listFile, 1));
+val splitedElement = splitter nthElement;*)
 
-val a = recursiv(listFile, []);
+val database = recursiv(listFile, []);
+
+val numeCandidat = getName(List.nth(database, 0));
+val partidCandidat = getParty(List.nth(database, 0));
+val voturi = getVotes(List.nth(database, 0));
+
+(* val isp = isPartyP ("Green" database); *)
+
+val sorted = sort database;
+
+(* val membersOf = filter (isPartyP ("Green" database)); *)
